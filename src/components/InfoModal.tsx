b2a7fb13b6388be6fa0a3d95a4a2731142d37377
stoppegp <ich@stoppe-gp.de>
2021-08-07 20:27:55 +0200

    InfoText hinzugefügt

--- src/components/InfoModal.tsx
+++ src/components/InfoModal.tsx
@@ -17,7 +17,7 @@ const InfoModal: React.FC = () => {
         <Modal.Header closeButton>
           <Modal.Title>ppig:ng</Modal.Title>
         </Modal.Header>
-        <Modal.Body>[todo]</Modal.Body>
+        <Modal.Body>Dieser Generator ist ein privates Angebot von <a href="https://twitter.com/stoppegp">@stoppegp</a> für Mitglieder und Sympathisanten der <a href="https://piratenpartei.de">Piratenpartei</a>. Die erstellten Bilder müssen im Einklang mit den <a href="https://wiki.piratenpartei.de/Parteiprogramm">Grundsätzen</a> der Piratenpartei sein.<br /><br />Die Verantwortung für die erstellten Bilder liegen ausschließlich beim Nutzer!<br /><br />Wünsche, Anregungen und Kritik gerne an <a href="mailto:piraten@stoppe-gp.de">piraten@stoppe-gp.de</a>.</Modal.Body>
         <Modal.Footer>
           <Button variant="primary" onClick={handleClose}>
             Schließen
